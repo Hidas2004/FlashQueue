81395bbbd47ebb16523822c787cf8ed1b610c6d6
hidas2004 <phihungbp1174@gmail.com>
2026-03-03 09:50:16 +0700

    fix(rabbitmq): hoàn thiện xử lý concurrency và dlq cho consumer

--- internal/queue/rabbitmq.go
+++ internal/queue/rabbitmq.go
@@ -1,7 +1,10 @@
 package queue
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -32,7 +35,7 @@ func (m *rabbitMQMessage) Nack(requeue bool) error {
 // 2. RABBITMQ CONNECTION MANAGER
 
 type RabbitMQ struct {
-	conn      *amqp.Connection
+	conn      *amqp.Connection //thứ sẽ được tái sử dụng để tạo channel mới cho publisher và consumer, giúp tiết kiệm tài nguyên và cải thiện hiệu suất
 	QueueName string
 	DLQName   string
 }
@@ -74,3 +77,92 @@ func NewRabbitMQFromURL(url, queueName, dlqName string) (*RabbitMQ, error) {
 }
 
 // 3. PUBLISHER IMPLEMENTATION
+type RabbitPublisher struct {
+	rmq *RabbitMQ
+	ch  *amqp.Channel
+}
+
+func NewPublisher(rmq *RabbitMQ) (*RabbitPublisher, error) {
+	ch, err := rmq.conn.Channel() //gọi channel từ kết nối có sẵn rmq.conn
+	if err != nil {
+		return nil, err
+	}
+	return &RabbitPublisher{
+		rmq: rmq,
+		ch:  ch,
+	}, nil
+}
+
+func (p *RabbitPublisher) Publish(ctx context.Context, msg interface{}) error {
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return p.ch.PublishWithContext(ctx, "", p.rmq.QueueName, false, false, amqp.Publishing{
+		ContentType:  "application/json",
+		DeliveryMode: amqp.Persistent,
+		Timestamp:    time.Now(),
+		Body:         body,
+	})
+
+}
+
+func (p *RabbitPublisher) PublishToDLQ(ctx context.Context, msg interface{}) error {
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return p.ch.PublishWithContext(ctx, "", p.rmq.DLQName, false, false, amqp.Publishing{
+		ContentType:  "application/json",
+		DeliveryMode: amqp.Persistent,
+		Timestamp:    time.Now(),
+		Body:         body,
+	})
+}
+func (p *RabbitPublisher) Close() error {
+	return p.ch.Close()
+}
+
+// 4. CONSUMER IMPLEMENTATION
+
+type RabbitConsumer struct {
+	rmq *RabbitMQ
+	ch  *amqp.Channel
+}
+
+func NewConSumer(rmq *RabbitMQ) (*RabbitConsumer, error) {
+	ch, err := rmq.conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	// Qos: Báo RabbitMQ chỉ đẩy 1 message cho worker này xong chờ Ack rồi mới đẩy tiếp
+	if err := ch.Qos(1, 0, false); err != nil {
+		return nil, err
+	}
+	return &RabbitConsumer{
+		rmq: rmq,
+		ch:  ch,
+	}, nil
+}
+
+// hàm tiêu thụ message
+func (c *RabbitConsumer) Consume() (<-chan Message, error) {
+	deliveries, err := c.ch.Consume(c.rmq.QueueName, "", false, false, false, false, nil)
+	if err != nil {
+		return nil, err
+	}
+	// Wrapper channel: Chuyển đổi từ amqp.Delivery sang interface Message của mình
+	msgCh := make(chan Message)
+	go func() {
+		// Goroutine này chạy ngầm để map data
+		for d := range deliveries { //lấy d từ channel deliveries, mỗi d là một amqp.Delivery
+			msgCh <- &rabbitMQMessage{delivery: d} //gửi một rabbitMQMessage mới vào msgCh, trong đó delivery được gán bằng d
+		}
+		close(msgCh)
+	}()
+	return msgCh, nil
+}
+
+func (c *RabbitConsumer) Close() error {
+	return c.ch.Close()
+}
